620ccb85a5aa848c22154477e8c22b7234d19ada
Nohemi Djebou <95258536+ndjebou@users.noreply.github.com>
2022-05-04 14:18:34 -0400

    add querry selector

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const firstRandomNum = Math.floor (Math.random()* 6) + 1
 //concatenation/images/dice1.png up to to dice6
 const firstDiceImage = "images/dice" + firstRandomNum + " .png";
 
+document.querySelectorAll
+("img") [0].setAttribute
+('src', firstDiceImage);
