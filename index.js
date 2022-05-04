6e4ea9f7cfbb73a19400d73ed15352f23152c7aa
Nohemi Djebou <95258536+ndjebou@users.noreply.github.com>
2022-05-04 10:36:07 -0400

    add index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+//Generate a random number from 1 to 6 
+const firstRandomNum = Math.floor (Math.random()* 6) + 1
+
+//concatenation/images/dice.png upto to dice6
+const firstDiceImage = "images/dice" + firstRandomNum + " .png";
+
