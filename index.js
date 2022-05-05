b2912f22d6d5e7e4e1415e83cdb1f4fb1ae65ea8
Nohemi Djebou <95258536+ndjebou@users.noreply.github.com>
2022-05-05 02:20:42 -0400

    corrected index.js code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //Generate a random number from 1 to 6 
 const firstRandomNum = Math.floor (Math.random()* 6) + 1
 
-//concatenation/images/dice1.png up to to dice6
-const firstDiceImage = "images/dice" + firstRandomNum + " .png";
+//concatenation/images/dice.png upto to dice6
+const firstDiceImage = 'images/dice' + firstRandomNum + '.png';
 
 document.querySelectorAll
-("img") [0].setAttribute
-('src', firstDiceImage);
+('img') [0].setAttribute
+('src', firstDiceImage); 
 
 
 const secondRandomNum = Math.floor (Math.random()* 6) + 1
