bdba654fa4193ace525c6736ac021034456b5197
Nohemi Djebou <95258536+ndjebou@users.noreply.github.com>
2022-05-05 01:01:47 -0400

    repeat process for second player

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const firstDiceImage = "images/dice" + firstRandomNum + " .png";
 document.querySelectorAll
 ("img") [0].setAttribute
 ('src', firstDiceImage);
+
+
+const secondRandomNum = Math.floor (Math.random()* 6) + 1
+
+const secondDiceImage = 'images/dice' + secondRandomNum + '.png';
+
+document.querySelectorAll
+('img') [1].setAttribute
+('src', secondDiceImage); 
